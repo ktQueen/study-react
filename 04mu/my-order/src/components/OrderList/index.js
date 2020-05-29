f84714297d396c4221335a2cc4d623eac7ba1d12
kangting <kangting@daojia-inc.com>
2020-05-30 07:22:18 +0800

    订单项组件

--- 04mu/my-order/src/components/OrderList/index.js
+++ 04mu/my-order/src/components/OrderList/index.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import OrderItem from '../OrderItem'
 
+const data={
+    id:1,
+    shop:'院落创意菜',
+    product:'红烧排骨',
+    price:'9.9',
+    ifCommented:true
+}
 class OrderList extends Component {
     render() {
         return (
             <div>
-                OrderList
+                <OrderItem data={data}/>
             </div>
         );
     }
