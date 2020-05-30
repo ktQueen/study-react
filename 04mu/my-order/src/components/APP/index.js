139f761b868879b14c65b72f4145648cb347db42
kangting <kangting@daojia-inc.com>
2020-05-30 08:16:44 +0800

    订单列表组件

--- 04mu/my-order/src/components/APP/index.js
+++ 04mu/my-order/src/components/APP/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './style.css';
 import OrderList from '../OrderList'
+import Header from '../Header'
 
 function App() {
   return (
     <div className="app">
+      <Header />
       <OrderList />
     </div>
   );
--- 04mu/my-order/src/components/OrderList/index.js
+++ 04mu/my-order/src/components/OrderList/index.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
 import OrderItem from '../OrderItem'
+import './style.css';
 
-const data={
-    id:1,
-    shop:'院落创意菜',
-    product:'红烧排骨',
-    price:'9.9',
-    ifCommented:true
-}
+const data=[
+    {
+        id:1,
+        shop:'院落创意菜',
+        product:'红烧排骨',
+        price:'9.9',
+        ifCommented:true
+    },
+    {
+        id:2,
+        shop:'院落创意菜',
+        product:'红烧带鱼',
+        price:'9.9',
+        ifCommented:false
+    },
+    {
+        id:3,
+        shop:'院落创意菜',
+        product:'凉拌黄瓜',
+        price:'9.9',
+        ifCommented:false
+    },
+    {
+        id:4,
+        shop:'院落创意菜',
+        product:'啤酒鸭',
+        price:'9.9',
+        ifCommented:false
+    }
+]
 class OrderList extends Component {
     render() {
         return (
-            <div>
-                <OrderItem data={data}/>
+            <div className="orderList">
+                {
+                    data.map((item)=>{
+                        return <OrderItem key={item.id} data={item}/>
+                    })
+                }
             </div>
         );
     }
