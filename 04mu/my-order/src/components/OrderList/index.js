4f94a44749cc9f762ed0370f01d87ed07d9c5a0b
kangting <kangting@daojia-inc.com>
2020-06-02 22:02:20 +0800

    模拟数据

--- 04mu/my-order/src/components/OrderList/index.js
+++ 04mu/my-order/src/components/OrderList/index.js
@@ -2,42 +2,30 @@ import React, { Component } from 'react';
 import OrderItem from '../OrderItem'
 import './style.css';
 
-const data=[
-    {
-        id:1,
-        shop:'院落创意菜',
-        product:'红烧排骨',
-        price:'9.9',
-        ifCommented:true
-    },
-    {
-        id:2,
-        shop:'院落创意菜',
-        product:'红烧带鱼',
-        price:'9.9',
-        ifCommented:false
-    },
-    {
-        id:3,
-        shop:'院落创意菜',
-        product:'凉拌黄瓜',
-        price:'9.9',
-        ifCommented:false
-    },
-    {
-        id:4,
-        shop:'院落创意菜',
-        product:'啤酒鸭',
-        price:'9.9',
-        ifCommented:false
-    }
-]
 class OrderList extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            data:[]
+        }
+    }
+    componentDidMount(){
+        // html5的fetch属性
+        fetch('/mock/orders.json').then(res=>{
+            if(res.ok){
+                res.json().then(data=>{
+                    this.setState({
+                        data
+                    })
+                })
+            }
+        })
+    }
     render() {
         return (
             <div className="orderList">
                 {
-                    data.map((item)=>{
+                    this.state.data.map((item)=>{
                         return <OrderItem key={item.id} data={item}/>
                     })
                 }
